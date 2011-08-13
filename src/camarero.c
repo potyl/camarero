9e829eb3a66b3db51baecd3859c8ac21bd2b685d
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2011-08-13 17:22:04 +0200

    The web server can return files
    It only returns files from the root file system and it slurps them into
    memory first.
    
    This is a very dumb web server.

--- src/camarero.c
+++ src/camarero.c
@@ -1,5 +1,77 @@
 #include <libsoup/soup.h>
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <string.h>
 
-int main (int argc, char ** argv) {
+
+static void
+server_callback (
+    SoupServer *server, SoupMessage *msg,
+    const char *path, GHashTable *query,
+    SoupClientContext *context, gpointer data
+) {
+
+    int status;
+
+    // Get the file size
+    struct stat st;
+    if (stat (path, &st) == -1) {
+        switch (errno) {
+            case EPERM:
+                status = SOUP_STATUS_FORBIDDEN;
+            break;
+
+            case ENOENT:
+                status = SOUP_STATUS_NOT_FOUND;
+            break;
+
+            default:
+                status = SOUP_STATUS_INTERNAL_SERVER_ERROR;
+            break;
+        }
+        goto DONE;
+    }
+
+    // Slurp the file and return it
+    int fd = open(path, O_RDONLY);
+    if (fd == -1) {
+        printf("Can't open %s; %s\n", path, strerror(errno));
+        status = SOUP_STATUS_INTERNAL_SERVER_ERROR;
+        goto DONE;
+    }
+    gchar *buffer = g_malloc(st.st_size);
+    read(fd, buffer, st.st_size);
+    close(fd);
+    soup_message_body_append(msg->response_body, SOUP_MEMORY_TAKE, buffer, st.st_size);
+    status = SOUP_STATUS_OK;
+
+    DONE:
+        soup_message_set_status(msg, status);
+        printf("Request for %s return status code %d\n", path, status);
+
+    return;
+}
+
+
+int
+main (int argc, char ** argv) {
+    g_thread_init(NULL);
+    g_type_init();
+
+    SoupServer *server = soup_server_new(
+        SOUP_SERVER_PORT, 3000,
+        SOUP_SERVER_SERVER_HEADER, "simple-httpd ",
+        NULL
+    );
+
+    soup_server_add_handler(server, NULL, server_callback, NULL, NULL);
+    printf("\nStarting Server on port %d\n", soup_server_get_port (server));
+    soup_server_run_async (server);
+
+    GMainLoop *loop = g_main_loop_new (NULL, TRUE);
+    g_main_loop_run(loop);
+
     return 0;
 }
