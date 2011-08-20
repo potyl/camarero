082ffd0cc9bb207593fe05d69ab914d83d224f34
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2011-08-20 22:52:13 +0200

    Use GPLv2 as the project's license

--- src/config.h
+++ src/config.h
@@ -1,2 +1,24 @@
+/*
+ * config.h
+ * This file is part of camarero
+ *
+ * Copyright (C) 2011 - Emmanuel Rodriguez
+ *
+ * camarero is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * camarero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libgit2-glib; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
 #define PACKAGE_NAME    "camarero"
 #define PACKAGE_VERSION "0.1"
